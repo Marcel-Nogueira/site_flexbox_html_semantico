823b8025992e3b91f0d19131ea14124e28f0fb45
Marcel Nogueira <nogueira.devmasters@gmail.com>
2024-08-29 16:15:52 -0300

    Add project_preview folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     document.querySelector ('.menu_mobile').addEventListener ('click', function (){
-       if (document.querySelector ('.menu nav ul').style.display == "flex" ){
+       if (document.querySelector ('.menu nav ul').style.display == 'flex' ){
         document.querySelector ('.menu nav ul').style.display = 'none';
 
        } else {
